f6549c52bd53a64bb6b9182c95d4b883a7b7bc49
bit-wise <bit-wise@users.noreply.github.com>
2020-03-02 12:18:11 -0500

    Update invert_mandel.js

--- invert_mandel.js
+++ invert_mandel.js
@@ -8,71 +8,53 @@ let s = 1; // Math.log10(S);
 
 function setup() {
   createCanvas(W, H);
-  background(255);
+  background(0);
 }
 
-let _r = 0;
-let _g = 0;
-let _b = 0;
-
 function draw() {
-  for (let i = 0; i < 100; i++) {
+  // background(255,255,255,1)
+  for (let i = 0; i < 10; i++) {
     // stroke(
     //   Math.round(Math.random() * 255),
     //   Math.round(Math.random() * 255),
     //   Math.round(Math.random() * 255),
-    //   1
     // );
-    stroke(
-      Math.round(_r),
-      Math.round(_g),
-      Math.round(_b),
-      16);
-    strokeWeight(s);
+    strokeWeight(0.25);
     noFill();
-    mandel().map(_m => {
+    mandel().map((_m, i) => {
+      stroke(255, 255, 255 ,i/10);
       point(_m[0] * S + W2, _m[1] * S + H2);
     });
-    // strokeWeight(0.25);
-    // beginShape();
-    // mandel().map(_m => {
-    //   vertex(_m[0] * S + W2, _m[1] * S + H2);
-    // });
-    // endShape();
   }
 }
 
-let j = -1;
-let k = -1;
-let _f = 0.0025;
+let l = 500 / S;
+let sr2 = Math.sqrt(2);
+let inc = 1;
+let j = -sr2;
+let k = -sr2;
 
 function mandel() {
-  _f = Math.random() / 10;
-  j += _f;
-  if (j > 1) {
-    j = -1;
-    k += _f;
+  j += inc;
+  if (j > sr2) {
+    j = -sr2;
+    k += inc;
   }
-  if (k > 1) {
-    k = -1;
+  if (k > sr2) {
+    k = -sr2;
+    inc = inc / 2;
   }
-  _r += _f;
-  if (_r > 245) {
-    _r = 0;
-    _g += _f;
-  }
-  if (_g > 245) {
-    _g = 0;
-    _b += _f;
-  }
-  if (_b > 245) {
-    _b = 0;
-  }
-  let cx = j; // Math.random() * 2 - 1;
-  let cy = k; // Math.random() * 2 - 1;
+  if(inc < 0.004){
+    saveCanvas('mandel', 'png')
+    noLoop();
+     }
+  cx = k;
+  cy = j;
+  // cx = Math.random() * (l * 2) - l;
+  // cy = Math.random() * (l * 2) - l;
   let z = [0, 0];
   let rz = [];
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1000; i++) {
     z = m([z[0], z[1]], [cx, cy]);
     rz.push(z);
   }
