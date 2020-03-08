6535662f572c31e89ec93aedb9ad3645f84dbeb6
bit-wise <bit-wise@users.noreply.github.com>
2020-03-07 19:27:50 -0500

    Update invert_mandel.js

--- invert_mandel.js
+++ invert_mandel.js
@@ -3,9 +3,8 @@ let W = 10000; // window.innerWidth;
 let H = W; // window.innerHeight;
 let H2 = H / 2;
 let W2 = W / 1.575;
-let F = (W/2000);
+let F = (W / 2000);
 let S = 850 * F; // scale
-let s = 1; // Math.log10(S);
 let ML = 1000;
 
 let sr2 = Math.sqrt(2);
@@ -21,29 +20,51 @@ function setup() {
   stroke(255);
 }
 
+let G = W / 10 / F;
+// G = 1;
+
 function draw() {
+  // G++;
   for (let j = 0; j < 100; j++) {
     mandel().map((_m, i) => {
-      let t = (i / ML) * 100;
-      stroke(255, 255, 255, t);
-      strokeWeight(t / (100 / F));
-      point(_m[0] * S + W2, _m[1] * S + H2);
+      if (i > 4) {
+        let t = (i / ML) * 100;
+        stroke(255, 255, 255);
+        strokeWeight(t / (100 / F));
+        point(_m[0] * S + W2, _m[1] * S + H2);
+      }
     });
   }
   I++;
   if (I > iMax) {
+    // saveCanvas('mandel', 'png');
+    // saveCanvas('mandel', 'jpg');
+    // noLoop();
+  }
+}
+
+let X = -sr2;
+let Y = -sr2;
+let XYF = 0.002;
+
+function mandel() {
+  // let cx = Math.round((Math.random() * sr22 - sr2) * G) / G;
+  // let cy = Math.round((Math.random() * sr22 - sr2) * G) / G;
+  X += XYF;
+  if (X > sr2) {
+    X = -sr2;
+    Y += XYF;
+  }
+  if (Y > sr2) {
+    Y = -sr2;
     saveCanvas('mandel', 'png');
     saveCanvas('mandel', 'jpg');
     noLoop();
   }
-}
+  
+  let cx = X;
+  let cy = Y;
 
-let G = W / 10 / F;
-
-function mandel() {
-
-  let cx = Math.round((Math.random() * sr22 - sr2) * G) / G;
-  let cy = Math.round((Math.random() * sr22 - sr2) * G) / G;
   let z = [0, 0];
   let rz = [];
   if (Math.sqrt(Math.pow(cx, 2) + Math.pow(cy, 2)) > sr2) {
@@ -52,10 +73,10 @@ function mandel() {
   for (let i = 0; i < ML; i++) {
     z = m([z[0], z[1]], [cx, cy]);
     rz.push(z);
-    if (rz.length == 4) {
-      let t = py(rz, 2, 1);
-      if (t >= sr22 || t <= 0) {
-        rz = [];
+    if (i >= 4) {
+      let t = py(rz, i - 3, i - 2);
+      if (t >= sr22) {
+        // rz = [];
         break;
       }
     }
